9d383d3ea2db59947e190c9c633302a77c868aca
Derek Dennison <dbdennison@ucdavis.edu>
2017-01-16 03:48:31 -0800

    Initial

---
+++ grunt/watch.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+    options: {
+        spawn: false,
+        livereload: true
+    },
+
+    css: {
+        files: [
+            'src/**/*.css'
+        ],
+    },
+	
+	html: {
+        files: [
+            'src/**/*.html'
+        ],
+    },
+	script: {
+        files: [
+            'src/**/*.js'
+        ],
+    },
+};
