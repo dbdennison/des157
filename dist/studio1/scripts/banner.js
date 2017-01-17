569065718fd04ace600b4914a5d0b5f0ed6d6591
Derek Dennison <dbdennison@ucdavis.edu>
2017-01-17 01:29:58 -0800

    cleanup

--- dist/studio1/scripts/banner.js
+++ dist/studio1/scripts/banner.js
@@ -6,23 +6,27 @@ var canvas = document.getElementById("banner"),
     canvas.width = canvas.offsetWidth;
 	canvas.height = canvas.offsetHeight;
 
-ctx.fillStyle='hsl(195, 82%, 9%)';
-	for (var i = 0; i < canvas.width / rectWidth; i++)
-		rects.push({x: i * rectWidth, y: 0, w: rectWidth, h: canvas.height});
+//fill rects with shapes depending on the size of the canvas
+for (var i = 0; i < canvas.width / rectWidth; i++)
+	rects.push({x: i * rectWidth, y: 0, w: rectWidth, h: canvas.height});
 
 canvas.onmousemove = function(eventUpdate) {
 
+	//get the x,y of the cursor relative to the canvas
 	var rect = this.getBoundingClientRect(),
 		x = eventUpdate.clientX - rect.left,
 		y = eventUpdate.clientY - rect.top,
 		i = 0, r;
   
+	//create drawing context
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-   
+	
+	//iterate through rects to draw each item
 	while(r = rects[i++]) {
 		ctx.beginPath();
 		ctx.rect(r.x, r.y, r.w, r.h);
-
+		
+		//check cursor for overlap of an element and set its fill
 		ctx.fillStyle = ctx.isPointInPath(x, y) ? "hsl(195, 82%, 9%)" : "hsl(195, 82%, 15%)";
 		ctx.fill();
 	}
